b1f3cc8952d2134238172936de4403dc2d458a4b
Artur Spychaj <drognanar@gmail.com>
2016-08-28 01:16:35 -0700

    Fix install template

--- profile/templates/module/install.ps1
+++ profile/templates/module/install.ps1
@@ -1,6 +1,6 @@
 # Install script.
 # Add any dependant modules that should be installed.
-if (-not [System.IO.Directory]::Exists($PROOT\Modules\so-posh)) {
+if (-not [System.IO.Directory]::Exists("$PROOT\Modules\so-posh")) {
   git clone https://github.com/drognanar/so-posh $PROOT\Modules\so-posh
   & $PROOT\Modules\so-posh\install.ps1
 }
